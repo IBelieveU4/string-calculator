f2b76c730d23d100e428a5c2ea8a913036e53956
IBelieveU4 <oleg.gorbay.02@gmail.com>
2022-10-24 18:54:22 +0300

    Second commit

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -12,7 +12,7 @@ import java.lang.Math;
 
 public class Calculator {
 
-    private static final int WINDOW_WIDTH = 410;
+    private static final int WINDOW_WIDTH = 400;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_WIDTH = 80;
     private static final int BUTTON_HEIGHT = 70;
